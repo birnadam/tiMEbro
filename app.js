6d0baafc4f8ac46d19fa2a8caa707de2d8697779
Garrett Young <GarrettYoung510@Gmail.com>
2019-10-05 17:35:28 -0700

    initial code for weather app

--- app.js
+++ app.js
@@ -193,6 +193,100 @@ search.addEventListener("keyup", () => {
 // --------------------TODO JS ENDS   HERE---------------------- //
 
 // -------------------WEATHER JS STARTS HERE-------------------- //
+class Forecast {
+  constructor() {
+    this.key = "IL0UncemSD2sDHJUo6hhT1ZW7YV94eN6";
+    this.weatherURI =
+      "https://dataservice.accuweather.com/currentconditions/v1/";
+    this.cityURI =
+      "https://dataservice.accuweather.com/locations/v1/cities/search";
+  }
+  async updateCity(city) {
+    const cityDets = await this.getCity(city);
+    const weather = await this.getWeather(cityDets.Key);
+
+    return {
+      cityDets,
+      weather
+    };
+  }
+  async getCity(city) {
+    const query = `?apikey=${this.key}&q=${city}`;
+    const response = await fetch(this.cityURI + query);
+    const data = await response.json();
+    //   console.log(data);
+    return data[0];
+  }
+  async getWeather(id) {
+    const query = `${id}?apikey=${this.key}`;
+    const response = await fetch(this.weatherURI + query);
+    const data = await response.json();
+    //   console.log(data);
+    return data[0];
+  }
+}
+
+// **** used for dom manipulation
+
+const cityForm = document.querySelector("form");
+const card = document.querySelector(".card");
+const details = document.querySelector(".details");
+const time = document.querySelector("img.time");
+const icon = document.querySelector(".icon img");
+const forecast = new Forecast();
+
+const updateUI = data => {
+  //   const cityDets = data.cityDets;
+  //   const weather = data.weather;
+
+  // destructure properties
+  const { cityDets, weather } = data;
+
+  // update details template
+  details.innerHTML = `
+    <h5 class="my-3">${cityDets.EnglishName}</h5>
+                <div class="my-3">${weather.WeatherText}</div>
+                <div class="display-4 my-4">
+                    <span>${weather.Temperature.Imperial.Value}</span>
+                    <span>&deg;F</span>
+                </div>
+    `;
+
+  // update images & icons
+  const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+  icon.setAttribute("src", iconSrc);
+
+  let timeSrc = weather.IsDayTime ? "img/day.svg" : "img/night.svg";
+
+  time.setAttribute("src", timeSrc);
+
+  // remove d-none class
+  if (card.classList.contains("d-none")) {
+    card.classList.remove("d-none");
+  }
+};
+
+cityForm.addEventListener("submit", e => {
+  e.preventDefault();
+
+  const city = cityForm.city.value.trim();
+  cityForm.reset();
+
+  // update the ui with new city
+  forecast.updateCity(city)
+    .then(data => updateUI(data))
+    .catch(err => console.log(err));
+
+  // set local storage
+  localStorage.setItem("city", city);
+});
+
+// if it exists automatically makes the api call from previous
+if (localStorage.getItem("city")) {
+  forecast.updateCity(localStorage.getItem("city"))
+    .then(data => updateUI(data))
+    .catch(err => console.log(err));
+}
 // -------------------WEATER JS ENDS   HERE--------------------- //
 
 // -------------------SETTINGS JS STARTS HERE------------------- //
